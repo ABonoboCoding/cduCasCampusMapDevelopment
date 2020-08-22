e2469b713908ab85e4de9ae1e3a328557a3eb28a
Minxuan Lin <James.lin1314520@outlook.com>
2020-08-22 09:48:02 +0930

    Upload of current progress
    Signed-off-by: Minxuan Lin <James.lin1314520@outlook.com>

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+// Initialize and add the map
+function initMap() {
+  //lat and lng of cdu campus
+  var cduCasCampus = {lat: -12.372, lng: 130.869};
+  // The map, centered at Casuarina Campus
+  //-12.371766, 130.868918
+
+  var cduCasuarinaCampusLIB = {lat: -12.371610, lng: 130.869377};
+  //Library longitude -12.371610 and latitude 130.869377
+
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: cduCasCampus});
+
+  //var cduCasCampusMarker = new google.maps.Marker({position: cduCasCampus, map: map});
+
+  //var cduCasLibMarker = new google.maps.Marker({position:cduCasuarinaCampusLIB, map: map, animation:google.maps.Animation.BOUNCE});
+
+  //define paths for cdu lib polygon
+  var cduCasCoords = [
+    {lat: -12.371339, lng: 130.869756}, //top right corner of lib -12.371339, 130.869756
+    {lat: -12.371222, lng: 130.869624}, //moving anti-clockwise corner, -12.371222, 130.869624
+    {lat: -12.371461, lng: 130.869405}, //-12.371461, 130.869405
+    {lat: -12.371201, lng: 130.869109}, //-12.371201, 130.869109
+    {lat: -12.371443, lng: 130.869118}, //-12.371443, 130.869118
+    {lat: -12.371592, lng: 130.869284}, //-12.371592, 130.869284
+    {lat: -12.371927, lng: 130.868976}, //-12.371927, 130.868976
+    {lat: -12.372042, lng: 130.869107} //-12.372042, 130.869107
+  ];
+
+  var cduLibPolygon= new google.maps.Polygon({
+  paths: cduCasCoords,
+  map: map,
+  strokeColor:"red",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"light red",
+  fillOpacity:0.4
+  });
+
+  cduLibPolygon.addListener('click', function (){
+    wi
+    e.location.href = 'https://www.cdu.edu.au/library';
+  });
+
+  if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+
+            var userIcon = {
+              url : 'https://img.icons8.com/dusk/16/000000/user-location.png', //LINK to icons: https://icons8.com/icons/set/user-16x16
+              size: new google.maps.Size(16, 16)
+            }
+
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            var userLocationMarker = new google.maps.Marker({position: pos, map: map, icon: userIcon});
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+}
