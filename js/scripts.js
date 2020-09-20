449231f21be0a69bae325a3e39c97cd2dfa2e086
Minxuan Lin <James.lin1314520@outlook.com>
2020-09-20 14:24:12 +0930

    Can receive coords from map. Added bin type display.
    Signed-off-by: Minxuan Lin <James.lin1314520@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 dataStorage = window.localStorage;
+
+var coordinatePickingMode = false;
 // Initialize and add the map
 function initMap() {
   //lat and lng of cdu campus
@@ -38,9 +40,9 @@ function initMap() {
   fillOpacity:0.4
   });
 
-  cduLibPolygon.addListener('click', function (){
-    window.location.href = 'https://www.cdu.edu.au/library';
-  });
+  //cduLibPolygon.addListener('click', function (){
+    //window.location.href = 'https://www.cdu.edu.au/library';
+  //});
 
   if (localStorage.length >= 1){
     for (var index = 0; index < localStorage.length; index++){
@@ -48,32 +50,80 @@ function initMap() {
       var locDetails = JSON.parse(localStorage.getItem(locName));
       var ltd = parseFloat(locDetails.ltd);
       var lng = parseFloat(locDetails.lng);
+      var rec = locDetails.rec;
+      var co = locDetails.co;
+      var gen = locDetails.gen;
+      var spe = locDetails.spe;
       var locCoords = {lat: ltd, lng: lng};
       console.log(locCoords);
 
-      if (locDetails.Type == "Recycling"){
-        var locName = new google.maps.Circle({
-          center: locCoords,
-          radius:10,
+      var locName = new google.maps.Circle({
+        center: locCoords,
+        radius:1,
+        map: map,
+        strokeColor: 'grey',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: 'grey',
+        fillOpacity: 0.35
+      });
+
+      if (rec == true){
+        recCoords = {lat: ltd + 0.000005, lng: lng}
+        var locNameRec = new google.maps.Circle({
+          center: recCoords,
+          radius:0.25,
           map: map,
           strokeColor: 'yellow',
           strokeOpacity: 0.8,
           strokeWeight: 2,
-          fillColor: 'light yellow',
-          fillOpacity: 0.35
-        });
-      } else if(locDetails.Type === "General"){
-        var locName = new google.maps.Circle({
-          center: locCoords,
-          radius:10,
-          map: map,
-          strokeColor: 'black',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: 'grey',
+          fillColor: 'yellow',
           fillOpacity: 0.35
         });
       }
+
+      if (co == true){
+        coCoords = {lat: ltd, lng: lng - 0.000005}
+        var locNameCo = new google.maps.Circle({
+          center: coCoords,
+          radius:0.25,
+          map: map,
+          strokeColor: 'red',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: 'yellow',
+          fillOpacity: 0.35
+        });
+      }
+
+      if (gen == true){
+        genCoords = {lat: ltd, lng: lng + 0.000005}
+        var locNameGen = new google.maps.Circle({
+          center: genCoords,
+          radius:0.25,
+          map: map,
+          strokeColor: 'green',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: 'green',
+          fillOpacity: 0.35
+        });
+      }
+
+      if (spe == true){
+        speCoords = {lat: ltd - 0.000005, lng: lng}
+        var locNamespe = new google.maps.Circle({
+          center: speCoords,
+          radius:0.25,
+          map: map,
+          strokeColor: 'blue',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: 'blue',
+          fillOpacity: 0.35
+        });
+      }
+
     }
   }
 
@@ -99,6 +149,14 @@ function initMap() {
           handleLocationError(false, infoWindow, map.getCenter());
         }
 
+        map.addListener('click', function(e) {
+          if (coordinatePickingMode === true){
+            GetCoordinatesFromMap(e.latLng, map);
+          } else {
+            // do nothing so that map can be operated normally
+          }
+        });
+
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
@@ -113,9 +171,14 @@ function displayAllLocations(){
   for (var index = 0; index < localStorage.length; index++){
     var locName = localStorage.key(index);
     var locDetails = JSON.parse(localStorage.getItem(locName));
-    var ltd = parseInt(locDetails.ltd);
-    var lng = parseInt(locDetails.lng);
-    var locCoords = {lat: -12.371575, lng: 130.868705};
+    var ltd = parseFloat(locDetails.ltd);
+    var lng = parseFloat(locDetails.lng);
+    var locCoords = {lat: ltd, lng: lng};
+    var rec = locDetails.rec;
+    var co = locDetails.co;
+    var gen = locDetails.gen;
+    console.log(rec);
+    return;
 
     var locName = new google.maps.Circle({
       center: locCoords,
@@ -136,11 +199,55 @@ function getInputValue(){
   var locName = document.getElementById("locNameInput").value;
   var ltdVal = document.getElementById("ltdInput").value;
   var lngVal = document.getElementById("lngInput").value;
-  var wasteType = document.getElementById("wasteType").value;
 
-  var locationDetails = {ltd: ltdVal, lng: lngVal, Type: wasteType};
+  var recycle = document.getElementById("recycle").checked;
+  var comingled = document.getElementById("comingled").checked;
+  var general = document.getElementById("general").checked;
+  var special = document.getElementById("special").checked;
+
+  var locationDetails = {ltd: ltdVal, lng: lngVal, rec: recycle, co: comingled, gen: general, spe: special};
   var locationDetailsJSON = JSON.stringify(locationDetails);
 
   localStorage.setItem(locName, locationDetailsJSON);
   initMap();
+}
+
+// Click event for coordinate picking
+
+
+
+// Function for getting coordinates from map
+function GetCoordinatesFromMap(latLng, map) {
+  //Optional code for if we want to add a marker on the map too
+  // var marker = new google.maps.Marker({
+  //   position: latLng,
+  //   map: map
+  // });
+  // map.panTo(latLng);
+  console.log("New map marker added, lat: " + latLng.lat() + " lng: " + latLng.lng());
+  // Insert picked coordinates to form fields
+  document.getElementById("ltdInput").value = latLng.lat();
+  document.getElementById("lngInput").value = latLng.lng();
+  // Disable coordinatePickingMode
+  coordinatePickingMode = false;
+  document.getElementById("pickFromMapBtn").innerHTML = "Retrieve coodinate from map";
+
+}
+
+//We only want to pick coordinates when button is pressed so we handle that with this function for now
+function checkPicking(){
+  if (coordinatePickingMode == true){
+    coordinatePickingMode = false;
+    document.getElementById("pickFromMapBtn").innerHTML = "Retrieve coodinate from map";
+    return;
+  }
+
+  if (coordinatePickingMode == false){
+    coordinatePickingMode = true;
+    document.getElementById("pickFromMapBtn").innerHTML = "Stop retrieving coodinate from map";
+    return;
+  }
+
+  console.log(coordinatePickingMode);
+
 }
